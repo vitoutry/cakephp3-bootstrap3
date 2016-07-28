2041aaa9bd02f20cdf95489ef66051bbff0bdb0b
Iván <dev@ivanamat.es>
2016-07-28 15:46:37 +0200

    config/app.php

---
+++ config/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'App' => [
+        'paths' => [
+            'templates' => [
+                APP . 'Template' . DS,
+                ROOT . DS . 'plugins' . DS .'Bootstrap3' .DS .'src' . DS . 'Template'
+            ]
+        ]
+    ]
+];
